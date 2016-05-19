548e3e5121fbe9c1bb4f2a741c94fe627035b457
poisson0106 <poisson0106@gmail.com>
2016-05-19 17:20:10 +0800

    add edge and node finished

--- Graph.py
+++ Graph.py
@@ -26,20 +26,35 @@ class Graph:
         tail = None
         for x in range(0, len(ary)):
             self.ary.append(Node(ary[x], x))
-        for x in range(0, len):
-            for y in range(x, len):
-                is_linked = raw_input("Is " + ary[y].val + " linked to" + ary[x].val + "? 1 or 0")
+        for x in range(0, self.len):
+            for y in range(x + 1, self.len):
+                is_linked = raw_input("Is " + self.ary[y].val + " linked to " + self.ary[x].val + "? 1 or 0\n")
                 if is_linked == "1":
-                    if ary[x].head is None:
-                        ary[x].head = Edge(x, y, ary[y].val)
-                    else:
-                        tmp = ary[x].head
-                        while tmp.next_edge is not None:
-                            tmp = tmp.next_edge
-                        tmp.next_edge = Edge(x, y, ary[y].val)
+                    self.add_edge(x, y, self.ary[y].val)
+                    self.add_edge(y, x, self.ary[x].val)
 
-    def add_edge(self,start,end val):
-        
+    def add_edge(self, start, end, val):
+        if self.ary[start].head is None:
+            self.ary[start].head = Edge(start, end, val)
+        else:
+            tmp = self.ary[start].head
+            while tmp.next_edge is not None:
+                tmp = tmp.next_edge
+            tmp.next_edge = Edge(start, end, val)
+
+    def add_node(self, val):
+        self.ary.append(Node(val, self.len))
+        self.len += 1
+
+    def remove_edge(self):
+        pass
+
+    def remove_node(self):
+        pass
 
     ary = []
     len = 0
+
+
+ary = ['a', 'e', 'q', 'y', 'x']
+graph = Graph(ary)
