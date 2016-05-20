5f4ebbff1944396644a227b9f05b9a33f284e3e8
poisson0106 <poisson0106@gmail.com>
2016-05-20 17:23:12 +0800

    remove edge ongoing

--- Graph.py
+++ Graph.py
@@ -46,11 +46,58 @@ class Graph:
         self.ary.append(Node(val, self.len))
         self.len += 1
 
-    def remove_edge(self):
-        pass
+    # This part needs to be tested
+    def remove_edge(self, ele1, ele2):
+        s_pos = -1
+        e_pos = -1
+        if self.has_edge(ele1, ele2):
+            s_pos = self.find_pos(ele1)
+            e_pos = self.find_pos(ele2)
+            self.remove_from_table(s_pos, e_pos)
+            self.remove_from_table(e_pos, s_pos)
+
+    def remove_from_table(self, s_pos, e_pos):
+        arrow = self.ary[s_pos].head
+        while arrow is not None:
+            if arrow.end == e_pos:
+                if arrow.next_edge is None:
+                    arrow = None
+                    break
+                else:
+                    arrow.end = arrow.next_edge.end
+                    arrow.val = arrow.next_edge.val
+                    arrow.next_edge = arrow.next_edge.next_edge
+                    break
+            else:
+                arrow = arrow.next_edge
 
     def remove_node(self):
         pass
+
+    def has_edge(self, start, end):
+        s_pos = self.find_pos(start)
+        e_pos = self.find_pos(end)
+        if s_pos == -1 or e_pos == -1:
+            return False
+        else:
+            first = self.ary[s_pos].head
+            result = False
+            if first is not None:
+                while first is not None:
+                    if first.val == self.ary[e_pos].val:
+                        result = True
+                        break
+                    else:
+                        first = first.next_edge
+
+            return result
+
+    def find_pos(self, ele):
+        pos = -1
+        for x in self.ary:
+            if x.val == ele:
+                pos = x.position
+        return pos
 
     ary = []
     len = 0
@@ -58,3 +105,4 @@ class Graph:
 
 ary = ['a', 'e', 'q', 'y', 'x']
 graph = Graph(ary)
+print (graph.has_edge('q', 'a'))
